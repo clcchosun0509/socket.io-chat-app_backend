b98cc71e5d23616515bd08aeddd9643bc320ec2a
박건우 <clcchosun0509@naver.com>
2022-11-28 08:20:31 +0900

    feat: add postgres store to session

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as session from 'express-session';
+import * as connectPgSimple from 'connect-pg-simple';
+import { Pool, PoolConfig } from 'pg';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -35,15 +37,29 @@ import { AuthModule } from './auth/auth.module';
 })
 export class AppModule {
   constructor(private readonly configService: ConfigService) {}
-  
+
   configure(consumer: MiddlewareConsumer) {
-    console.log(this.configService.get<string>('SESSION_SECRET'))
+    const pgSession = connectPgSimple(session);
+    const pgPoolConfig: PoolConfig = {
+      database: this.configService.get<string>('DB_NAME'),
+      user: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      port: this.configService.get<number>('DB_PORT'),
+      max: 10,
+      idleTimeoutMillis: 1000,
+      connectionTimeoutMillis: 1000,
+    };
+
     consumer
       .apply(
         session({
           secret: this.configService.get<string>('SESSION_SECRET'),
           resave: false,
           saveUninitialized: false,
+          store: new pgSession({
+            pool: new Pool(pgPoolConfig),
+            createTableIfMissing: true,
+          }),
         }),
       )
       .forRoutes('*');
